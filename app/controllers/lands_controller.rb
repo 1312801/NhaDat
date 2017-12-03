1e272cfebb2ed9fa2614e86775346b2d332507c6
1312801 <1312801@student.hcmus.edu.vn>
2017-12-03 08:44:17 +0700

    Cool

--- app/controllers/lands_controller.rb
+++ app/controllers/lands_controller.rb
@@ -3,5 +3,24 @@ class LandsController < ApplicationController
     @lands=Land.all
   end
   def new
+    @land=Land.new
+  end
+  def show
+    @land= Land.find(params[:id])
+  end
+  def create
+@land=Land.new(land_params)
+if @land.save
+  flash[:success]="Ban dang ki thanh cong"
+  redirect_to lands_path
+else
+  flash[:error]="Dang ki lai !!"
+  render 'new'
+end
+  end
+  private
+  def land_params
+    params.require(:user).permit(:TenDuAn, :TenChuDauTu, :Phuong, :Quan, :ThanhPho , :Tinh, :Dientich,:DiaChi,:SoPhongNgu,:SoTang)
+
   end
 end
